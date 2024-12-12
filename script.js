d0c27241c9b4142427653690aab2854a2bff2f1a
Ishwar Singh Bhandari <ishwarsinghbhandari46@gmail.com>
2024-12-12 00:45:19 -0500

    first

---
+++ script.js
@@ -0,0 +1,91 @@
+
+
+async function fetchAllProducts() {
+    try {
+
+        const response = await fetch("https://fakestoreapi.com/products");
+        
+        const products = await response.json();
+
+        console.log("Products fetched from API:", products);
+        
+        const productList = document.getElementById("product-list");
+        products.forEach(product => {
+            const productItem = document.createElement("div");
+            productItem.className = "card col-4 m-2 ";
+            productItem.style.width = "18rem";
+            productItem.innerHTML = `
+            
+            <img src="${product.image} " class="card-img-top img-fluid " alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${product.title}</h5>
+                 <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${product.category}</li>
+                    <li class="list-group-item">${product.price}</li>
+                </ul>
+                <button onClick="SingleProduct(${product.id})" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">Watch Now</button>
+            </div>
+       
+        `
+
+            productList.appendChild(productItem);
+        });
+
+    } catch (error) {
+        console.error("Error fetching products:", error);
+    }
+}
+document.addEventListener("DOMContentLoaded", fetchAllProducts);
+
+
+async function SingleProduct(id){
+    
+    try {
+
+        const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+        
+        const products = await response.json();
+
+        console.log("Single Products fetched from API:", products);
+
+        const productList = document.getElementById("model3");
+
+        productList.innerHTML = "";
+            
+            const productItem = document.createElement("div");
+            productItem.className = "card mb-3 p-2";
+            productItem.style.width = "100%";
+            productItem.innerHTML = `
+            
+              <div class="row g-0">
+            <div class="col-md-4">
+              <img src="${products.image}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${products.title}</h5>
+                   <div class="d-flex justify-content-between">
+                <p class="card-text fs-4 text-bold">${products.price} $</p>
+                <p class="me-3 fs-6">In Stock: ${products.rating.count} </p>
+                </div>
+                <div class="d-flex justify-content-between">
+                    <p class=" fs-6">Category: ${products.category} </p>
+                    <p class="me-3 fs-6">Rating: ${products.rating.rate}/5 </p>
+                    
+                </div>
+                <p class="card-text">${products.description} </p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 hr ago</small></p>
+              </div>
+            </div>
+          </div>
+       
+        `
+
+            productList.appendChild(productItem);
+       
+
+}
+catch (error) {
+    console.error("Error fetching products:", error);
+}
+}
